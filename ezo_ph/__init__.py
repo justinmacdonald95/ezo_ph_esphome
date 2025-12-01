a36e293a99d0caadd200b4ef086cbc2a9137876a
J/// <justin.macdonald95@gmail.com>
2025-12-01 11:26:51 -0400

    json

--- ezo_ph/__init__.py
+++ ezo_ph/__init__.py
@@ -1,2 +1,5 @@
-# Required for ESPHome external components
-from . import sensor
+"""EZO pH sensor external component for ESPHome.
+
+This is required for ESPHome to recognize the component.
+"""
+# No Python code needed for this simple component
